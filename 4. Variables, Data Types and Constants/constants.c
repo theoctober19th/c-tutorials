e288cd0da59a6d8d9f46d324692d90bc6087d153
Bikalpa Dhakal <theoctober19th@gmail.com>
2020-04-21 08:16:17 +0545

    completed variables, data types and constants

---
+++ 4. Variables, Data Types and Constants/constants.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define PI 3.14
+
+int main()
+{
+
+    printf("Value of Pi is %f", PI);
+
+    return 0;
+}
---
+++ 4. Variables, Data Types and Constants/variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+    float rateOfInterest = 6.25;
+    unsigned int age;
+
+    age = 23;
+
+    printf("The value of age is: %u \n", age);
+    printf("The value of rate is: %f \n", rateOfInterest);
+
+    return 0;
+}
