d4e622a6d7c0c0fb103a12215636fa6e1b7b20bb
Bikalpa Dhakal <theoctober19th@gmail.com>
2020-05-02 22:28:21 +0545

    switch case

---
+++ 17. switch/switch.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char digit = 'a';
+
+    switch (digit)
+    {
+    case '0':
+        printf("zero\n");
+        break;
+    case '1':
+        printf("one\n");
+        break;
+    case '2':
+        printf("two\n");
+        break;
+    case '3':
+        printf("three\n");
+        break;
+    case '4':
+        printf("four\n");
+        break;
+    case '5':
+        printf("five\n");
+        break;
+    case '6':
+        printf("six\n");
+        break;
+    case '7':
+        printf("seven\n");
+        break;
+    case '8':
+        printf("eight\n");
+        break;
+    case '9':
+        printf("nine\n");
+        break;
+    default:
+        printf("not a digit\n");
+    }
+
+    return 0;
+}
