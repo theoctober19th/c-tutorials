27e2ffd41ccdd998dd58021ab6f58a4a321a75b1
Bikalpa Dhakal <theoctober19th@gmail.com>
2020-05-27 15:31:43 +0545

    44 Recursion

---
+++ 44. Recursion/factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main()
+{
+    int n = 5;
+    printf("5! = %d\n", factorial(5));
+}
