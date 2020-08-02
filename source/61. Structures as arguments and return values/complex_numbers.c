63c201ffcd7e8b65d4938ce2a996e926c79dc628
Bikalpa Dhakal <theoctober19th@gmail.com>
2020-08-02 13:09:35 +0545

    changed expression for complex number multiplication

--- source/61. Structures as arguments and return values/complex_numbers.c
+++ source/61. Structures as arguments and return values/complex_numbers.c
@@ -1,35 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
 
-
-struct complex{
+struct complex
+{
     double real;
     double img;
 };
 
-struct complex add(struct complex c1, struct complex c2){
+struct complex add(struct complex c1, struct complex c2)
+{
     struct complex res;
     res.real = c1.real + c2.real;
     res.img = c1.img + c2.img;
     return res;
 }
 
-struct complex multiply(struct complex c1, struct complex c2){
+struct complex multiply(struct complex c1, struct complex c2)
+{
     struct complex product;
-    product.real = c1.real * c2.img - c1.img * c2.real;
-    product.img = c1.img * c2.real + c1.real * c2.img;
+    product.real = c1.real * c2.real - c1.img * c2.img;
+    product.img = c1.real * c2.img + c2.real * c1.img;
     return product;
 }
 
-void print(struct complex n){
+void print(struct complex n)
+{
     printf("%lf + i %lf\n", n.real, n.img);
 }
 
-int main(){
+int main()
+{
 
     struct complex c1 = {1, 2}, c2 = {5.5, 6.7};
     struct complex sum, product;
-    sum  = add(c1, c2);
-    print(sum);
+    sum = add(c1, c2);
     product = multiply(c1, c2);
+    print(sum);
     print(product);
 }
