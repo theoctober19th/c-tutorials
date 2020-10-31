956a638f5ed2099a6096796ec4df7a596062a3df
Bikalpa Dhakal <theoctober19th@gmail.com>
2020-10-31 10:46:27 +0545

    rewritten coding exercise 24

--- source/80. Coding Exercise #24/dynamic_array.c
+++ source/80. Coding Exercise #24/dynamic_array.c
@@ -1,130 +1,138 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//a pointer that points to the dynamic array
+// this pointer points to the dynamic array
 int *array = NULL;
-//the size of dynamic array
+
+//this tracks the size of dynamic array
 int size = 0;
 
+
 void init();
-void add(int, int);
-void delete (int);
+void insert(int, int);
+void _remove(int);
 void display();
 void cleanup();
 
-int main()
-{
+int main() {
+    
+    int index, number;
+    int choice = 0;
+
     init();
-    char choice;
-    int index, value;
-    do
-    {
-        printf("MAIN MENU\n");
+
+    do{
+        
         printf("1. Insert an element at a specified index.\n");
-        printf("2. Delete an element from a specified index.\n");
-        printf("3. Display the dynamic array.\n");
-        printf("4. Exit the program\n");
-        printf("Enter your choice: ");
-        scanf(" %c", &choice);
-        switch (choice)
-        {
-        case '1':
-            printf("Enter the index and the value: ");
-            scanf("%d%d", &index, &value);
-            add(index, value);
-            break;
-        case '2':
-            printf("Enter the index: ");
-            scanf("%d", &index);
-            delete (index);
-            break;
-        case '3':
-            display();
-            break;
-        case '4':
-            exit(0);
-            break;
+        printf("2. Remove an element from a specified index.\n");
+        printf("3. Display the elements in the array.\n");
+        printf("4. Exit\n");
+
+        printf("Enter your choice.\n");
+        scanf("%d", &choice);
+
+        switch(choice){
+            case 1: 
+                printf("Enter index and element: ");
+                scanf("%d %d", &index, &number);
+                insert(index, number);
+                break;
+            case 2:
+                printf("Enter the index: ");
+                scanf("%d", &index);
+                _remove(index);
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                cleanup();
+                exit(0);
+            default:
+                printf("Enter a valid choice.\n");
+                break;
         }
-    } while (1);
-    cleanup();
+    } while(1);
+    
     return 0;
 }
 
-void init()
-{
-    printf("Enter the initial size of dynamic array: ");
-    scanf("%d", &size);
+void init(){
+    int n;
+    printf("Enter the initial size of the array: ");
+    scanf("%d", &n);
+    size = n;
 
-    array = malloc(size * sizeof(int));
-    if (array == NULL)
-    {
-        printf("Allocation failed\n");
+    array = malloc(n * sizeof(int));
+    if(array == NULL){
+        printf("The allocation failed!\n");
         exit(1);
     }
 
-    printf("Enter %d elements: ", size);
+    printf("Enter %d elements: ", n);
     int i;
-    for (i = 0; i < size; i++)
-    {
+    for(i=0; i<n; i++){
         scanf("%d", &array[i]);
     }
 }
 
-void cleanup()
-{
-    if (array != NULL)
-    {
+void cleanup(){
+    if(array != NULL){
         free(array);
     }
 }
 
-void add(int index, int value)
-{
+void display(){
+    int i;
+    printf("The elements are: ");
+    for(i=0; i<size; i++){
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+void insert(int index, int element){
+    if(index >= size || index < 0){
+        printf("Invalid index provided.\n");
+        return;
+    }
     size++;
+
     int *temp = realloc(array, size * sizeof(int));
-    if (temp == NULL)
-    {
-        printf("Realloation failed!\n");
+    if(temp == NULL){
+        printf("Reallocation failed! \n");
         cleanup();
         exit(2);
     }
+
     array = temp;
-    int prev = array[index];
     int i;
-    for (i = index; i < size; i++)
-    {
-        int current = array[i];
-        array[i] = prev;
-        prev = current;
+
+    for(i=size-1; i>index; i--){
+        array[i] = array[i-1];
     }
-    array[index] = value;
+
+    array[index] = element;
 }
 
-void delete (int index)
-{
+void _remove(int index){
+    if(index >= size || index < 0){
+        printf("Invalid index provided.\n");
+        return;
+    }
+
     size--;
     int i;
-    for (i = index; i < size; i++)
-    {
-        array[i] = array[i + 1];
+
+    for(i=index; i<size; i++){
+        array[i] = array[i+1];
     }
+
     int *temp = realloc(array, size * sizeof(int));
-    if (temp == NULL)
-    {
-        printf("Reallocation Failed!\n");
+    if(temp == NULL){
+        printf("Reallocation failed!\n");
         cleanup();
         exit(3);
     }
     array = temp;
-}
-
-void display()
-{
-    int i;
-    printf("The elements in the array are: ");
-    for (i = 0; i < size; i++)
-    {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
 }
