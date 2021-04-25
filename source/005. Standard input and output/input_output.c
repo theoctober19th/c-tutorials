f0476a090fb684f650aa4318bcde666e0593e24e
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2021-04-25 19:24:54 +0545

    removed -(dash) from the scanf

--- source/005. Standard input and output/input_output.c
+++ source/005. Standard input and output/input_output.c
@@ -7,7 +7,7 @@ int main()
 
     int number1, number2;
 
-    scanf("%c%d-%d", &my_char, &number1, &number2);
+    scanf("%c%d%d", &my_char, &number1, &number2);
 
     printf("MyChar = %c \nNumber1 = %10d \nNumber2 = %d", my_char, number1, number2);
 
